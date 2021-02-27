a9d9da209b9807176fb695efd702329ada59aa7e
Luis <luisclaudiocr1@gmail.com>
2021-02-27 13:17:32 -0300

    Profile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -7,7 +7,7 @@ export function Profile() {
 
   return (
     <div className={styles.profileContainer}>
-      <img src="https://github.com/luisclaudiocr" alt="Foto Perfil"/>
+      <img src="https://media-exp1.licdn.com/dms/image/C5603AQH5RLNByioRyg/profile-displayphoto-shrink_200_200/0/1613504012123?e=1619654400&v=beta&t=b4XSoQsSDh8FPnj9JeuzVrgU1thEr3EVuuf_0uStJqc" alt="Foto Perfil"/>
       <div>
         <strong>Luís Cláudio</strong>
         <p><img src="icons/level.svg" alt="Level"/>
